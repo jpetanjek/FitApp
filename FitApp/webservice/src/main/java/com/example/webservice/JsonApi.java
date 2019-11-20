b19f0cc5e3dc5d19024a30ac71dee868ad8def12
jpetanjek <jpetanjek@foi.hr>
2019-11-20 19:41:08 +0100

    Ispravak unosa novog korisnika pri registraciji i u web servisu #32

--- FitApp/webservice/src/main/java/com/example/webservice/JsonApi.java
+++ FitApp/webservice/src/main/java/com/example/webservice/JsonApi.java
@@ -6,7 +6,9 @@ import RetroEntities.RetroKorisnik;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -19,4 +21,24 @@ public interface JsonApi {
 
     @POST("korisnik.php?query=update")
     Call<RetroKorisnik> azurirajKorisnika(@Field("identifikator") int idKorisnika, @Field("atribut") String atribut, @Field("vrijednost") String vrijednost);
+
+    @Multipart
+    @POST("korisnik.php?query=insert")
+    Call<Void> unesiKorisnika (@Part("ime") String ime,
+                               @Part("prezime") String prezime,
+                               @Part("google_id") String googleId,
+                               @Part("email") String email,
+                               @Part("visina") Float visina,
+                               @Part("razina_aktivnosti") Integer razina_aktivnosti,
+                               @Part("cilj_mase") Integer ciljMase,
+                               @Part("cilj_tjednog_mrsavljenja") Float ciljTjednogMrsavljenja,
+                               @Part("spol") String spol,
+                               @Part("datum_rodenja") String datumRodenja);
+
+    @Multipart
+    @POST("korisnik.php?query=update")
+    Call<Void> azuriranjeKorisnika(@Part("identifikator") Integer identifikator,
+                                   @Part("atribut") String atribut,
+                                   @Part("vrijednost") String vrijednost);
+
 }

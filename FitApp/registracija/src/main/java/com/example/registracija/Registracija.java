98dfb812e633821114eb1524fa31256d56c5edfe
Sabina <spintar01@gmail.com>
2019-11-20 20:24:59 +0100

    Popravljano sučelje za registraciju #9

--- FitApp/registracija/src/main/java/com/example/registracija/Registracija.java
+++ FitApp/registracija/src/main/java/com/example/registracija/Registracija.java
@@ -7,29 +7,34 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class Registracija extends AppCompatActivity {
 
-    TextView email;
     ImageView imageView;
+    TextView email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registracija);
 
-
+        //imageView = findViewById(R.id.profile_image);
+        imageView = findViewById(R.id.profile_image);
         email = findViewById(R.id.tvMail);
-        imageView = findViewById(R.id.IvMail);
 
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+
         if (acct != null) {
             String personEmail = acct.getEmail();
             Uri personPhoto = acct.getPhotoUrl();
 
+
             email.setText(personEmail);
+            Glide.with(this).load(String.valueOf(personPhoto)).into(imageView);
         }
     }
 

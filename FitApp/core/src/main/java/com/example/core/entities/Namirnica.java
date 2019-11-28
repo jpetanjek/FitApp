dafb9267e7d793db45bad7a0e1cab6ba0c932841
Jurica Migač <jurica1.migac@gmail.com>
2019-11-29 00:42:21 +0100

    Dodana klasa Namirnica, entitet lokalne baze. #44 Dodana nova klasa Namirnica koja sadrži sve atribute kao i istovjetna na web servisu. Uz osnovne funkcije gettera i settera dodana je funkcija parsiranja, pomoću koje se može Response oblik (RetroNamirnica) pretvoriti u tip čitljiv za lokalnu bazu podataka.

---
+++ FitApp/core/src/main/java/com/example/core/entities/Namirnica.java
@@ -0,0 +1,70 @@
+package com.example.core.entities;
+
+import androidx.annotation.Nullable;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import RetroEntities.RetroNamirnica;
+import retrofit2.Response;
+
+@Entity(tableName = "namirnica")
+public class Namirnica {
+    @PrimaryKey
+    private int id;
+    
+    private String naziv;
+    private int brojKalorija;
+    private int tezina;
+
+    @Nullable
+    private String isbn;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getBrojKalorija() {
+        return brojKalorija;
+    }
+
+    public void setBrojKalorija(int brojKalorija) {
+        this.brojKalorija = brojKalorija;
+    }
+
+    public int getTezina() {
+        return tezina;
+    }
+
+    public void setTezina(int tezina) {
+        this.tezina = tezina;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public Namirnica parseNamirnica(Response<RetroNamirnica> response){
+        Namirnica novaNamirnica = new Namirnica();
+        novaNamirnica.setId(response.body().getId());
+        novaNamirnica.setNaziv(response.body().getNaziv());
+        novaNamirnica.setTezina(response.body().getTezina());
+        novaNamirnica.setBrojKalorija(response.body().getBrojKalorija());
+        novaNamirnica.setIsbn(response.body().getIsbn());
+        return novaNamirnica;
+    }
+}

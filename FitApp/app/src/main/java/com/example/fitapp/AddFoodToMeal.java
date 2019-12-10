39a11dc2626e98d317a92cac84d69973d5296242
Jurica Migač <jurica1.migac@gmail.com>
2019-12-10 13:41:00 +0100

    Dodano pozivanje i prikazivanje fragmenata na AddFoodToMeal. #12 #25 - Dodan FrameLayout u XML - Dodane ovisnosti o modulima u app - Dodano pozivanje fragmenata pritiskom na gumb

--- FitApp/app/src/main/java/com/example/fitapp/AddFoodToMeal.java
+++ FitApp/app/src/main/java/com/example/fitapp/AddFoodToMeal.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -15,10 +14,9 @@ import android.widget.ImageView;
 
 import com.example.barkod.fragments.BarkodFragment;
 import com.example.core.entities.Namirnica;
-import com.example.fitapp.adapters.FoodDiaryAdapter;
 import com.example.fitapp.adapters.NamirniceAdapter;
-import com.example.fitapp.adapters.NamirniceObrokaAdapter;
 import com.example.repository.NamirnicaDAL;
+import com.example.unos_hrane.Add_new_food;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +26,6 @@ import adapter.CurrentActivity;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
 
 public class AddFoodToMeal extends AppCompatActivity {
 
@@ -37,6 +34,7 @@ public class AddFoodToMeal extends AppCompatActivity {
     private RecyclerView recyclerView;
     private String nazivObroka;
     private Bundle prosljedeniPodaci;
+    private Button unosNoveNamirnice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,26 +42,35 @@ public class AddFoodToMeal extends AppCompatActivity {
         setContentView(R.layout.activity_add_food_to_meal);
         slikaBarkoda = findViewById(R.id.picBarcode);
         nazivNamirnice = findViewById(R.id.txtNazivNamirnice);
+        unosNoveNamirnice = findViewById(R.id.btnUnosNoveNamirnice);
+
         prosljedeniPodaci = getIntent().getExtras();
         nazivObroka = prosljedeniPodaci.getString("Obrok");
         CurrentActivity.setActivity(AddFoodToMeal.this);
         slikaBarkoda.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent i = new Intent(AddFoodToMeal.this,BarkodSkenerActivity.class);
-                i.putExtras(prosljedeniPodaci);
-                startActivity(i);
-
+                unosNoveNamirnice.setVisibility(View.INVISIBLE);
+                BarkodFragment barkodFragment = new BarkodFragment();
+                barkodFragment.setArguments(prosljedeniPodaci);
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("BarkodFragment")
+                        .replace(R.id.fragmentModul,barkodFragment)
+                        .commit();
             }
         });
-        Button button = findViewById(R.id.button3);
-        button.setOnClickListener(new View.OnClickListener() {
+        unosNoveNamirnice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Intent i = new Intent(AddFoodToMeal.this,ModularActivity.class);
-                //i.putExtras(prosljedeniPodaci);
-                //startActivity(i);
+
+                Add_new_food anfFragment = new Add_new_food();
+                anfFragment.setArguments(prosljedeniPodaci);
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("AddNewFood")
+                        .replace(R.id.fragmentModul,anfFragment)
+                        .commit();
             }
         });
     }
@@ -112,5 +119,6 @@ public class AddFoodToMeal extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         popuniSadrzajPretrage();
+        unosNoveNamirnice.setVisibility(View.VISIBLE);
     }
 }

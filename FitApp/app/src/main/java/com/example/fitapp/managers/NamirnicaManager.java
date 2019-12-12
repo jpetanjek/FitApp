74d7fa822657676ef9fafdbc231c94f6997a8305
Jurica Migač <jurica1.migac@gmail.com>
2019-12-12 12:04:19 +0100

    Kreiran upravitelj rada nad modularnim fragmentima. #12 #25 - Omoućuje pokretanja pojedinog fragmenta. - Nužno je da prosljeđeni fragment ima bundle kao argument.

---
+++ FitApp/app/src/main/java/com/example/fitapp/managers/NamirnicaManager.java
@@ -0,0 +1,39 @@
+package com.example.fitapp.managers;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.barkod.fragments.BarkodFragment;
+import com.example.fitapp.R;
+
+import managers.NamirnicaImporter;
+
+public class NamirnicaManager {
+    private static NamirnicaManager INSTANCE;
+    private AppCompatActivity activity;
+
+    private NamirnicaManager(){
+
+    }
+
+    public static NamirnicaManager getINSTANCE() {
+        if(INSTANCE==null){
+            INSTANCE = new NamirnicaManager();
+        }
+        return INSTANCE;
+    }
+    public void setupManager(AppCompatActivity activity){
+
+        this.activity = activity;
+    }
+    public void startModule(NamirnicaImporter importer){
+        activity.getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentModul,importer.getFragment())
+                .addToBackStack(importer.getName())
+                .commit();
+    }
+
+}

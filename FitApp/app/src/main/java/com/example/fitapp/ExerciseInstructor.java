a6146f0fb5bf99e2d62eef627d8ce8f2e136b64c
mmaric <mmaric@foi.hr>
2019-12-19 20:43:35 +0100

    Implementirano zapisivanje podataka o konfiguraciji vježbe u lokalnu bazu (KorisnikVjezba i AtributiVjezbeSnage) i prosljeđivanje unesenih ID u aktivnost ExerciseInstructor.  #122

--- FitApp/app/src/main/java/com/example/fitapp/ExerciseInstructor.java
+++ FitApp/app/src/main/java/com/example/fitapp/ExerciseInstructor.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.core.entities.KorisnikVjezba;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExerciseInstructor extends AppCompatActivity {
     private String nazivVjezbe;
+    private List<Integer> listaKorisnikVjezbeId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +22,16 @@ public class ExerciseInstructor extends AppCompatActivity {
 
         nazivVjezbe = getIntent().getExtras().getString("nazivVjezbe");
         Toolbar toolbar = findViewById(R.id.toolbar1);
-        toolbar.setTitle(toolbar.getTitle() + nazivVjezbe);
+        //toolbar.setTitle(toolbar.getTitle() + nazivVjezbe);
 
+        listaKorisnikVjezbeId = getIntent().getIntegerArrayListExtra("listaKorisnikVjezbeId");
+        String naziv = toolbar.getTitle().toString();
+        for(int item: listaKorisnikVjezbeId){
+            naziv = naziv.concat( " id:");
+            naziv = naziv.concat(Integer.toString(item));
+            Log.v("UNESENO2", Integer.toString(item));
+
+        }
+        toolbar.setTitle(naziv);
     }
 }
